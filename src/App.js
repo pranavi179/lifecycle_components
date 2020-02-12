7a7b1767ff619147b22b4a22dbbaa9394cdc6a98
Pranavi <48429437+pranavi179@users.noreply.github.com>
2020-02-12 13:32:30 +0530

    Added lifecycle methods
    Life cycles methods such as componentDidmount and componentdidupdate included

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React from "react";
 import "./styles.css";
 import Counter from "./counter";
 
-export default function App() {
-  return (
-    <div className="App">
-      {/* <h1>Hello CodeSandbox</h1>
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mount: true
+    };
+    this.mountCounter = () => this.setState({ mount: true });
+    this.unmountCounter = () => this.setState({ mount: false });
+  }
+  render() {
+    return (
+      <div className="App">
+        {/* <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2> */}
-      <Counter />
-    </div>
-  );
+        <button onClick={this.mountCounter} disabled={this.state.mount}>
+          Mount Counter
+        </button>
+        <button onClick={this.unmountCounter} disabled={!this.state.mount}>
+          Unmount COunter
+        </button>
+
+        <Counter />
+      </div>
+    );
+  }
 }
